d8aac315d5d12ebb55ae43ce75262625ed900284
Jundullah H. R <jundihanif77@gmail.com>
2021-01-16 13:48:30 +0700

    [Jundi] Tambah konfirmasi buat arrow key

--- src/class/com/pbo/wws/io/KeyMapper.java
+++ src/class/com/pbo/wws/io/KeyMapper.java
@@ -17,35 +17,35 @@ public class KeyMapper extends KeyAdapter{
 	public static final int KEY_ESCAPE = 9;
 
 	private static boolean[] keyPressedFlag = new boolean[10];
-	private static boolean   enterConfirmed = false, escapeConfirmed = false;
+	private static boolean   enterConfirmed = false, escapeConfirmed = false, arrowConfirmed = false;
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		switch (e.getKeyCode()) {
 		case KeyEvent.VK_UP:
 		case KeyEvent.VK_KP_UP:
-			if (!keyPressedFlag[KEY_UP]) {
+			if (!keyPressedFlag[KEY_UP] && !arrowConfirmed) {
 				keyPressedFlag[KEY_UP] = true;
 				System.out.println("[KeyMapper] UP");				
 			}
 			break;
 		case KeyEvent.VK_DOWN:
 		case KeyEvent.VK_KP_DOWN:
-			if (!keyPressedFlag[KEY_DOWN]) {
+			if (!keyPressedFlag[KEY_DOWN] && !arrowConfirmed) {
 				keyPressedFlag[KEY_DOWN] = true;
 				System.out.println("[KeyMapper] DOWN");				
 			}
 			break;
 		case KeyEvent.VK_LEFT:
 		case KeyEvent.VK_KP_LEFT:
-			if (!keyPressedFlag[KEY_LEFT]) {
+			if (!keyPressedFlag[KEY_LEFT] && !arrowConfirmed) {
 				keyPressedFlag[KEY_LEFT] = true;
 				System.out.println("[KeyMapper] LEFT");				
 			}
 			break;
 		case KeyEvent.VK_RIGHT:
 		case KeyEvent.VK_KP_RIGHT:
-			if (!keyPressedFlag[KEY_RIGHT]) {
+			if (!keyPressedFlag[KEY_RIGHT] && !arrowConfirmed) {
 				keyPressedFlag[KEY_RIGHT] = true;
 				System.out.println("[KeyMapper] RIGHT");				
 			}
@@ -97,21 +97,25 @@ public class KeyMapper extends KeyAdapter{
 		case KeyEvent.VK_UP:
 		case KeyEvent.VK_KP_UP:
 			keyPressedFlag[KEY_UP] = false;
+			arrowConfirmed = false;
 			System.out.println("[KeyMapper] UP RELEASED");
 			break;
 		case KeyEvent.VK_DOWN:
 		case KeyEvent.VK_KP_DOWN:
 			keyPressedFlag[KEY_DOWN] = false;
+			arrowConfirmed = false;
 			System.out.println("[KeyMapper] DOWN RELEASED");
 			break;
 		case KeyEvent.VK_LEFT:
 		case KeyEvent.VK_KP_LEFT:
 			keyPressedFlag[KEY_LEFT] = false;
+			arrowConfirmed = false;
 			System.out.println("[KeyMapper] LEFT RELEASED");
 			break;
 		case KeyEvent.VK_RIGHT:
 		case KeyEvent.VK_KP_RIGHT:
 			keyPressedFlag[KEY_RIGHT] = false;
+			arrowConfirmed = false;
 			System.out.println("[KeyMapper] RIGHT RELEASED");
 			break;
 		case KeyEvent.VK_W:
@@ -153,12 +157,17 @@ public class KeyMapper extends KeyAdapter{
 		System.out.println("[KeyMapper] [KeyMapper] Key yang kamu cek tidak ada");
 		return false;
 	}
-	
+
 	public static void confirmEnter() {
 		enterConfirmed = true;
 	}
-	
+
 	public static void confirmEscape() {
 		escapeConfirmed = true;
+	}
+	
+	
+	public static void confirmArrow() {
+		arrowConfirmed = true;
 	}
 }
